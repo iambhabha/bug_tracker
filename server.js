e169cb8b76d989c194dbadaab77d88afc7e9a9d9
iambhabha <kapilsharma@gtsinfosoft.com>
2026-03-25 12:06:05 +0530

    chang code

--- server.js
+++ server.js
@@ -169,4 +169,7 @@ app.post("/migrate-to-trello", async (req, res) => {
     }
 });
 
-app.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
